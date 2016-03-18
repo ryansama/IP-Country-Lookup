b53c73d697c38d892469b5d64be357257859f285
Ryan Samarajeewa <ryan.samarajeewa@gmail.com>
2016-03-18 15:33:20 -0600

    Add sqlite with sample query

--- program.py
+++ program.py
@@ -5,7 +5,23 @@
 #Description
 #   A web app / program that offers a quick way to map a given IP address to a country.
 
+import sqlite3
 def main():
-    print 'hello'
+    db_connection = sqlite3.connect('test.db')
+    createTable(db_connection)
+
+def createTable(conn):
+    conn = sqlite3.connect('test.db')
+    print "Opened database successfully"
+
+    conn.execute('''CREATE TABLE COMPANY
+           (ID INT PRIMARY KEY     NOT NULL,
+           NAME           TEXT    NOT NULL,
+           AGE            INT     NOT NULL,
+           ADDRESS        CHAR(50),
+           SALARY         REAL);''')
+    print "Table created successfully"
+
+    conn.close()
 
 main()
