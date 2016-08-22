88788ac81115a8c067569094e0420c3f784fb0a3
Ryan Samarajeewa <ryan.samarajeewa@gmail.com>
2016-08-22 15:07:15 -0600

    Finish Page Scraper

--- Web/page-scraper.py
+++ Web/page-scraper.py
@@ -1,12 +1,20 @@
 import urllib2
 import sys
+import lxml.html
 
-if len(sys.argv) != 2:
-    print "Usage: python page-scraper.py <url>"
-else:
-    try:
-        response = urllib2.urlopen(sys.argv[1])
-        html = response.read()
-        print html
-    except:
-        print "Invalid url."
+# returns string
+def get_page():
+    if len(sys.argv) != 2:
+        print "Usage: python page-scraper.py <url>"
+        exit()
+    else:
+        try:
+            response = urllib2.urlopen(sys.argv[1])
+            return response.read()
+        except:
+            print "Invalid url."
+            exit()
+
+dom =  lxml.html.fromstring(get_page())
+for link in dom.xpath('//a/@href'): # select the url in href for all a tags(links)
+    print link
