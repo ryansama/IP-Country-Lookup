0c39dac7a50267c70964b1d25bab7af43edeb158
Ryan Samarajeewa <ryan.samarajeewa@gmail.com>
2016-08-24 10:10:00 -0600

    Add exception handling for file output

--- Web/page-scraper.py
+++ Web/page-scraper.py
@@ -1,7 +1,11 @@
+#usage: python page-scraper.py <valid url>
+#required: Python 2.7
+
 import urllib2
 import sys
 import lxml.html
 
+url_input = ""
 # returns html as string
 def get_page():
     if len(sys.argv) != 2:
@@ -9,6 +13,8 @@ def get_page():
         exit()
     else:
         try:
+            global url_input
+            url_input = sys.argv[1]
             response = urllib2.urlopen(sys.argv[1])
             return response.read()
         except:
@@ -19,6 +25,12 @@ dom =  lxml.html.fromstring(get_page())
 links = set(dom.xpath('//a/@href'))
 
 #add url to relative page links
-f = open('list-of-links.txt', 'w')
-for link in links:
-    f.write(link + '\n')    
+
+try:
+    f = open('list-of-links.txt', 'w')
+    f.write('url: ' + url_input + '\n')
+    f.write('--------------------------------\n')
+    for link in links:
+        f.write(link + '\n')
+except:
+    print "Could not create or write to file."    
